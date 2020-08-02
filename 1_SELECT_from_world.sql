dc5a4e3889fd3213af737fb2c01b6f1c17cf2fd4
Dylan Williams <happyfrog578+github@gmail.com>
2020-08-03 00:40:09 +1000

    finish part 1

--- 1_SELECT_from_world.sql
+++ 1_SELECT_from_world.sql
@@ -85,6 +85,21 @@ WHERE
   LENGTH(name) = LENGTH(capital);
 
 -- 12. Matching name and capital
-
+SELECT
+  name, capital
+FROM
+  world
+WHERE
+  LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
 
 -- 13. All the vowels
+SELECT name
+   FROM world
+WHERE
+  name NOT LIKE '% %'
+  AND name LIKE '%a%'
+  AND name LIKE '%e%'
+  AND name LIKE '%i%'
+  AND name LIKE '%o%'
+  AND name LIKE '%U%';
+
