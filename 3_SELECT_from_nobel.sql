be76e91a071b075ef4f3b0b954fa1516c3d5ce18
chook <dylanw212@gmail.com>
2020-08-04 17:58:59 +1000

    nobel complete

--- 3_SELECT_from_nobel.sql
+++ 3_SELECT_from_nobel.sql
@@ -39,19 +39,77 @@ WHERE
 yr BETWEEN 1980 AND 1989 AND subject = 'literature';
 
 -- 6. Show all details of the presidential winners:
+SELECT
+  *
+FROM
+  nobel
+WHERE
+  winner IN ('Theodore Roosevelt',
+             'Woodrow Wilson',
+             'Jimmy Carter',
+             'Barack Obama');
 
 -- 7. Show the winners with first name John
+SELECT
+  winner
+FROM
+  nobel
+WHERE
+  winner LIKE 'John%';
 
 -- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT
+  yr, subject, winner
+FROM
+  nobel
+WHERE
+  (yr = 1980 AND subject = 'Physics') OR (yr = 1984 AND subject = 'chemistry');
 
 -- 9. Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+SELECT
+  yr, subject, winner
+FROM
+  nobel
+WHERE
+  yr = 1980 AND subject NOT IN ('medicine', 'chemistry');
 
 -- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year
+SELECT
+  yr, subject, winner
+FROM
+  nobel
+WHERE
+  subject = 'medicine' AND yr < 1910 OR subject = 'literature' AND yr > 2003;
 
 -- 11. Find all details of the prize won by PETER GRÜNBERG
+SELECT
+  *
+FROM
+  nobel
+WHERE
+  winner = 'PETER GRÜNBERG';
 
 -- 12. Find all details of the prize won by EUGENE O'NEILL
+SELECT
+  *
+FROM
+  nobel
+WHERE
+  winner = 'EUGENE O''NEILL';
 
 -- 13. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
-
+SELECT
+  winner, yr, subject
+FROM
+  nobel
+WHERE
+  winner LIKE 'sir%'
+ORDER BY
+  yr DESC, winner;
 -- 14. Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE
+   yr = 1984
+ ORDER BY
+   subject IN ('Physics','Chemistry'), subject, winner;
