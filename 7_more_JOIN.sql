d6bbccbafd7705532d83e17faea923d3561f4f31
chook <dylanw212@gmail.com>
2020-08-05 21:36:58 +1000

    7 mostly complete

--- 7_more_JOIN.sql
+++ 7_more_JOIN.sql
@@ -1,26 +1,70 @@
--- 1.
+-- 1. List the films where the yr is 1962 [Show id, title]
+SELECT id, title
+FROM movie
+WHERE yr=1962
 
--- 2.
+-- 2. Give year of 'Citizen Kane'.
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane';
 
--- 3.
+-- 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%star trek%'
+ORDER BY yr;
 
--- 4.
+-- 4. What id number does the actor 'Glenn Close' have?
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close';
 
--- 5.
+-- 5. What is the id of the film 'Casablanca'
+SELECT id
+FROM movie
+WHERE title = 'Casablanca';
 
--- 6.
+-- 6. Obtain the cast list for 'Casablanca'.
+SELECT name
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie on movieid = movie.id
+WHERE movieid = 11768;
 
--- 7.
+-- 7. Obtain the cast list for the film 'Alien'
+SELECT name
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie on movieid = movie.id
+WHERE title = 'Alien';
 
--- 8.
+-- 8. List the films in which 'Harrison Ford' has appeared
+SELECT title
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie on movieid = movie.id
+WHERE name = 'Harrison Ford';
 
--- 9.
+-- 9. List the films where 'Harrison Ford' has appeared - but not in the starring role.
+SELECT title
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie on movieid = movie.id
+WHERE name = 'Harrison Ford' AND ord != 1;
 
--- 10.
+-- 10. List the films together with the leading star for all 1962 films.
+SELECT title, name
+FROM actor JOIN casting ON actor.id = actorid
+JOIN movie on movieid = movie.id
+WHERE yr = 1962 AND ord = 1;
 
--- 11.
+-- Harder Questions
 
--- 12.
+-- 11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+        JOIN actor   ON actorid=actor.id
+WHERE name='Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2;
+
+-- 12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
 
 -- 13.
 
