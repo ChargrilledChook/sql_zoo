3a13c0c16cab4ab013e778bc64a3628fbddc8b21
chook <dylanw212@gmail.com>
2020-08-05 13:29:14 +1000

    add working files

---
+++ 6_JOIN.sql
@@ -0,0 +1,29 @@
+-- 1. Modify it to show the matchid and player name for all goals scored by Germany.
+SELECT matchid, player
+FROM goal
+WHERE teamid = 'GER';
+
+-- 2. Show id, stadium, team1, team2 for just game 1012
+
+
+-- 3.
+
+-- 4.
+
+-- 5.
+
+-- 6.
+
+-- 7.
+
+-- 8.
+
+-- 9.
+
+-- 10.
+
+-- 11.
+
+-- 12.
+
+-- 13.
